9c3d93769c400cdcfe42a20a5d53c4e0ec73582e
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2018-12-09 04:20:19 +0300

    add GA

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,18 @@ export default class MyDocument extends Document {
             <html>
                 <Head>
                     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-130596321-1" />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+    
+                        gtag('config', 'UA-130596321-1');
+                    `,
+                        }}
+                    />
                     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet" />
                     <link href="https://unpkg.com/firacode@1.205.0/distr/fira_code.css" rel="stylesheet" />
                 </Head>
